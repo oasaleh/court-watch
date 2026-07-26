d334db667de4bed3ee5de56fc0ffedf23b0cf920
Omar Saleh <56449814+oasaleh@users.noreply.github.com>
2026-07-26 10:21:53 -0500

    Initial Commit

---
+++ CourtWatch/CourtWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourtWatchApp.swift
+//  CourtWatch
+//
+//  Created by Omar Saleh on 7/26/26.
+//
+
+import SwiftUI
+
+@main
+struct CourtWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CourtWatchTests/CourtWatchTests.swift
@@ -0,0 +1,19 @@
+//
+//  CourtWatchTests.swift
+//  CourtWatchTests
+//
+//  Created by Omar Saleh on 7/26/26.
+//
+
+import Testing
+@testable import CourtWatch
+
+struct CourtWatchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
